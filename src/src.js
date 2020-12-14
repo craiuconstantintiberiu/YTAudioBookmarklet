fbe5c8f5e37ae8e257c7b8fde692699f439d5373
Constantin-Tiberiu Craiu <57532657+craiuconstantintiberiu@users.noreply.github.com>
2020-12-14 08:40:56 +0200

    Update src.js

--- src/src.js
+++ src/src.js
@@ -1,20 +1,13 @@
 javascript :(function() {
-var YTBsoundBoost=prompt("By how much do you want to multiply the audio compared to initial?","2")//default value - 2
+  var YTBsoundBoost=prompt("By how much do you want to multiply the audio compared to initial?","2"); 
+  if(typeof YTBgainNode ==='undefined')
+  {
+      var YTBourVideo = document.querySelector("video");
+      var YTBaudioCtx = new AudioContext();
+      var YTBsource = YTBaudioCtx.createMediaElementSource(YTBourVideo); 
+      var YTBgainNode = YTBaudioCtx.createGain(); YTBgainNode.gain.value = YTBsoundBoost; 
+      YTBsource.connect(YTBgainNode); YTBgainNode.connect(YTBaudioCtx.destination);
+  } 
+  else YTBgainNode.gain.value=YTBsoundBoost;
 
-var YTBourVideo = document.querySelector("video");
-
-var YTBaudioCtx = new AudioContext();
-
-var YTBsource = YTBaudioCtx.createMediaElementSource(videoElement);
-
-var YTBgainNode = YTBaudioCtx.createGain();
-
-YTBgainNode.gain.value = YTBsoundBoost
-
-YTBsource.connect(gainNode)
-
-YTBgainNode.connect(audioCtx.destination)
-
-//Not much to see here
-//Put YTB in front of variables since I read a prefix should avoid having a variable with same name existing on page I change audio for.
 }());
