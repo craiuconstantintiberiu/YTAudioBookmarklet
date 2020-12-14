018be956d3b951c54325f0a665da00af7fadcd9e
Constantin-Tiberiu Craiu <57532657+craiuconstantintiberiu@users.noreply.github.com>
2020-12-14 09:00:18 +0200

    Update src.js

--- src/src.js
+++ src/src.js
@@ -1,13 +1,13 @@
-javascript :(function() {
-  var YTBsoundBoost=prompt("By how much do you want to multiply the audio compared to initial?","2"); 
-  if(typeof YTBgainNode ==='undefined')
-  {
-      var YTBourVideo = document.querySelector("video");
-      var YTBaudioCtx = new AudioContext();
-      var YTBsource = YTBaudioCtx.createMediaElementSource(YTBourVideo); 
-      var YTBgainNode = YTBaudioCtx.createGain(); YTBgainNode.gain.value = YTBsoundBoost; 
-      YTBsource.connect(YTBgainNode); YTBgainNode.connect(YTBaudioCtx.destination);
-  } 
-  else YTBgainNode.gain.value=YTBsoundBoost;
-
-}());
+javascript: (function() {
+    var YTBsoundBoost = prompt("By how much do you want to multiply the audio compared to initial?", "2");
+    if (typeof YTBgainNode === 'undefined') {
+        YTBourVideo = document.querySelector("video");
+        YTBaudioCtx = new AudioContext();
+        YTBsource = YTBaudioCtx.createMediaElementSource(YTBourVideo);
+        YTBgainNode = YTBaudioCtx.createGain();
+        YTBgainNode.gain.value = YTBsoundBoost;
+        YTBsource.connect(YTBgainNode);
+        YTBgainNode.connect(YTBaudioCtx.destination);
+    }
+    YTBgainNode.gain.value = YTBsoundBoost;
+}())
